3bdc0753c75364ffaab5f6db932c49db556d7b69
Vlad Mayik <vladislavmayik@gmail.com>
2026-05-05 15:46:11 +0200

    Update

--- src/main/java/com/solvd/bankingapp/dao/factory/ConnectionFactory.java
+++ src/main/java/com/solvd/bankingapp/dao/factory/ConnectionFactory.java
@@ -7,6 +7,8 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.io.Reader;
@@ -14,13 +16,14 @@ import java.io.Reader;
 public class ConnectionFactory {
 
     private static SqlSessionFactory factory;
+    private static final Logger LOGGER = LogManager.getLogger(ConnectionFactory.class);
 
     static {
         Reader reader = null;
         try {
             reader = Resources.getResourceAsReader("mybatis-config.xml");
         } catch (IOException e) {
-            throw new RuntimeException(e.getMessage());
+            LOGGER.error("Failed to initialize MyBatis connection factory", e);
         }
         factory = new SqlSessionFactoryBuilder().build(reader);
     }
